4f596b0c09b5b0371a92ba69bd10dc76df9ecc21
benja-amb1 <benjaminrodriguezambroselli@outlook.com.ar>
2025-12-02 22:56:10 -0300

    fix: agregación de env en emailconfig.ts

--- backend/src/utils/emailconfig.ts
+++ backend/src/utils/emailconfig.ts
@@ -6,8 +6,8 @@ const PASS = process.env.EMAIL_PASS;
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: USER,
-    pass: PASS
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 })
 
