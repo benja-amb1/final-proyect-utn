e50d5f290245c4a4846b29de9caadf381ecd4255
benja-amb1 <benjaminrodriguezambroselli@outlook.com.ar>
2025-12-02 23:11:32 -0300

    add: ruta exacta de las imagenes

--- backend/src/middlewares/upload.middleware.ts
+++ backend/src/middlewares/upload.middleware.ts
@@ -3,7 +3,7 @@ import path from 'node:path';
 import fs from 'node:fs';
 import crypto from 'crypto';
 
-const uploadDir = path.join(__dirname, '../../uploads');
+const uploadDir = path.join(__dirname, '../uploads');
 
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
